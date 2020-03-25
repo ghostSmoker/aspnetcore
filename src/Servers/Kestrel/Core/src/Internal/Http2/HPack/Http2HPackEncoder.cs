72dc3156e42a7e88ed0be4599246caf0b4b8b37e
James Newton-King <james@newtonking.com>
2020-03-25 17:45:38 +1300

    Make encoder sharable with runtime

--- src/Servers/Kestrel/Core/src/Internal/Http2/HPack/Http2HPackEncoder.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http2/HPack/Http2HPackEncoder.cs
@@ -6,7 +6,6 @@ using System;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.HPack;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
 {
@@ -170,14 +169,11 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
             var currentLength = 0;
             do
             {
-                var name = headersEnumerator.Current.Key;
-                var value = headersEnumerator.Current.Value;
-
                 if (!EncodeHeader(
                     buffer.Slice(currentLength),
-                    GetResponseHeaderStaticTableId(headersEnumerator.KnownHeaderType),
-                    name,
-                    value,
+                    headersEnumerator.HPackStaticTableId,
+                    headersEnumerator.Current.Key,
+                    headersEnumerator.Current.Value,
                     out var headerLength))
                 {
                     // If the header wasn't written, and no headers have been written, then the header is too large.
@@ -411,63 +407,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
         private int CalculateBucketIndex(int hash)
         {
             return hash & _hashMask;
-        }
-
-        internal static int GetResponseHeaderStaticTableId(KnownHeaderType responseHeaderType)
-        {
-            switch (responseHeaderType)
-            {
-                case KnownHeaderType.CacheControl:
-                    return H2StaticTable.CacheControl;
-                case KnownHeaderType.Date:
-                    return H2StaticTable.Date;
-                case KnownHeaderType.TransferEncoding:
-                    return H2StaticTable.TransferEncoding;
-                case KnownHeaderType.Via:
-                    return H2StaticTable.Via;
-                case KnownHeaderType.Allow:
-                    return H2StaticTable.Allow;
-                case KnownHeaderType.ContentType:
-                    return H2StaticTable.ContentType;
-                case KnownHeaderType.ContentEncoding:
-                    return H2StaticTable.ContentEncoding;
-                case KnownHeaderType.ContentLanguage:
-                    return H2StaticTable.ContentLanguage;
-                case KnownHeaderType.ContentLocation:
-                    return H2StaticTable.ContentLocation;
-                case KnownHeaderType.ContentRange:
-                    return H2StaticTable.ContentRange;
-                case KnownHeaderType.Expires:
-                    return H2StaticTable.Expires;
-                case KnownHeaderType.LastModified:
-                    return H2StaticTable.LastModified;
-                case KnownHeaderType.AcceptRanges:
-                    return H2StaticTable.AcceptRanges;
-                case KnownHeaderType.Age:
-                    return H2StaticTable.Age;
-                case KnownHeaderType.ETag:
-                    return H2StaticTable.ETag;
-                case KnownHeaderType.Location:
-                    return H2StaticTable.Location;
-                case KnownHeaderType.ProxyAuthenticate:
-                    return H2StaticTable.ProxyAuthenticate;
-                case KnownHeaderType.RetryAfter:
-                    return H2StaticTable.RetryAfter;
-                case KnownHeaderType.Server:
-                    return H2StaticTable.Server;
-                case KnownHeaderType.SetCookie:
-                    return H2StaticTable.SetCookie;
-                case KnownHeaderType.Vary:
-                    return H2StaticTable.Vary;
-                case KnownHeaderType.WWWAuthenticate:
-                    return H2StaticTable.WwwAuthenticate;
-                case KnownHeaderType.AccessControlAllowOrigin:
-                    return H2StaticTable.AccessControlAllowOrigin;
-                case KnownHeaderType.ContentLength:
-                    return H2StaticTable.ContentLength;
-                default:
-                    return -1;
-            }
         }
     }
 }
